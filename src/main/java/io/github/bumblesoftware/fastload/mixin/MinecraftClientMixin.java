58d4756822567edd995021b6de90b2e6e7ed86f9
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-06-12 01:52:53 +1000

    integrate mcclientmixin

--- src/main/java/io/github/bumblesoftware/fastload/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/MinecraftClientMixin.java
@@ -2,14 +2,30 @@ package io.github.bumblesoftware.fastload.mixin;
 
 import net.minecraft.client.MinecraftClient;
 
+import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
+import net.minecraft.client.gui.screen.Screen;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin {
     @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;tick()V"), argsOnly = true)
-    private boolean render(boolean tick) {
-        return true;
+    public final boolean render(boolean tick) {return true;}
+
+    @Shadow
+    public void setScreen(@Nullable Screen screen) {}
+
+    @Inject(at = @At("HEAD"), method = "setScreen", cancellable = true)
+    public void setScreen(final Screen screen, final CallbackInfo ci) throws InterruptedException {
+        if (screen instanceof DownloadingTerrainScreen) {
+            Thread.sleep(400);
+            ci.cancel();
+            setScreen(null);
+        }
     }
 }
