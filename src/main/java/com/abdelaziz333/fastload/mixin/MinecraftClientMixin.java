01dbe1f31253353b303396dfd0677a905777a6fc
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-06-13 09:28:40 +0200

    x1.19 forge port

---
+++ src/main/java/com/abdelaziz333/fastload/mixin/MinecraftClientMixin.java
@@ -0,0 +1,55 @@
+package com.abdelaziz333.fastload.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
+import net.minecraft.client.gui.screen.Screen;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+    //Original code is from 'kennytv, forceloadingscreen'. Code is modified to our needs.
+    @Shadow
+    public void setScreen(@Nullable Screen screen) {}
+    @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
+    private void setScreen(final Screen screen, final CallbackInfo ci) {
+        if (screen instanceof DownloadingTerrainScreen) {
+            render(true);
+            ci.cancel();
+            setScreen(null);
+            justLoaded = true;
+        }
+    }
+
+    @Shadow
+    private void render(boolean tick) {}
+
+    @Shadow
+    public boolean skipGameRender;
+
+    @Shadow private boolean windowFocused;
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void setSkipGameRender(boolean tick, CallbackInfo ci) {
+        skipGameRender = false;
+    }
+
+    private boolean justLoaded;
+
+    @Shadow private volatile boolean running;
+
+    @Inject(method = "openPauseMenu", at = @At("HEAD"), cancellable = true)
+    private void cancelOpenPauseMenu(boolean pause, CallbackInfo ci) {
+        if (windowFocused && justLoaded) {
+            justLoaded = false;
+        }
+        else if (!windowFocused && running && justLoaded) {
+            ci.cancel();
+            justLoaded = false;
+        }
+    }
+}
---
+++ src/main/java/com/abdelaziz333/fastload/mixin/MinecraftServerMixin.java
@@ -0,0 +1,21 @@
+package com.abdelaziz333.fastload.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.server.MinecraftServer;
+
+
+/*
+* This code is inspired by: https://github.com/VidTu/Ksyxis of which it's under the MIT License.
+* The BumbleSoftware team modified the code to make this possible.
+*/
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @ModifyConstant(method = "prepareStartRegion", constant = @Constant(intValue = 441))
+    public int onPrepareRedirectChunksLoaded(int value) {
+        return 49;
+    }
+}
