87e1ad112688ed5dd767fde73400539fbe41da24
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-06-14 20:08:28 +1000

    yep, this mixin is even bigger now

--- src/main/java/io/github/bumblesoftware/fastload/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/MinecraftClientMixin.java
@@ -1,12 +1,10 @@
 package io.github.bumblesoftware.fastload.mixin;
 
+import io.github.bumblesoftware.fastload.MinecraftClientMixinInterface;
 import net.minecraft.client.MinecraftClient;
 
 import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.resource.ResourcePackManager;
-import net.minecraft.server.SaveLoader;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,17 +13,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public abstract class MinecraftClientMixin {
+public class MinecraftClientMixin implements MinecraftClientMixinInterface {
     //Original code is from 'kennytv, forceloadingscreen'. Code is modified to our needs.
     @Shadow
     public void setScreen(@Nullable Screen screen) {}
     @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
     private void setScreen(final Screen screen, final CallbackInfo ci) {
-        if (screen instanceof DownloadingTerrainScreen) {
+        if (screen instanceof DownloadingTerrainScreen && shouldLoad && playerJoined) {
             render(true);
             ci.cancel();
             setScreen(null);
             justLoaded = true;
+            shouldLoad = false;
+            playerJoined  = false;
         }
     }
     @Shadow
@@ -42,16 +42,28 @@ public abstract class MinecraftClientMixin {
     private void setSkipGameRender(boolean tick, CallbackInfo ci) {
         skipGameRender = false;
     }
-    private boolean justLoaded;
+    private boolean justLoaded = false;
     @Shadow private volatile boolean running;
     @Inject(method = "openPauseMenu", at = @At("HEAD"), cancellable = true)
     private void cancelOpenPauseMenu(boolean pause, CallbackInfo ci) {
-        if (windowFocused && justLoaded) {
-            justLoaded = false;
+        if(justLoaded) {
+            if (windowFocused) {
+                justLoaded = false;
+            }
+            else if (running) {
+                ci.cancel();
+                justLoaded = false;
+            }
         }
-        else if (!windowFocused && running && justLoaded) {
-            ci.cancel();
-            justLoaded = false;
-        }
+    }
+    private boolean shouldLoad = false;
+    @Override
+    public void canPlayerLoad() {
+        shouldLoad = true;
+    }
+    private boolean playerJoined = false;
+    @Override
+    public void gameJoined() {
+        playerJoined = true;
     }
 }
