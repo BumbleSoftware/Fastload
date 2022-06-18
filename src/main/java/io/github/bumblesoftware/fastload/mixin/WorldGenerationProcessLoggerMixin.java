001235d29b95fe311a2d15ad67734657b54131bf
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-06-18 22:02:16 +1000

    fix c2me compat

--- src/main/java/io/github/bumblesoftware/fastload/mixin/WorldGenerationProcessLoggerMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/WorldGenerationProcessLoggerMixin.java
@@ -1,14 +1,26 @@
 package io.github.bumblesoftware.fastload.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
 import net.minecraft.server.WorldGenerationProgressLogger;
 
-@Mixin(WorldGenerationProgressLogger.class)
+
+@Mixin(value = WorldGenerationProgressLogger.class, priority = 1200)
 public class WorldGenerationProcessLoggerMixin {
+    @Shadow @Final @Mutable private int totalCount = 81;
     @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
-    private static int injected(int radius){
+    private static int setRadius(int radius) {
         return 4;
+    }
+    @Shadow private int generatedCount;
+    /**
+     * @author Fluffy Bumblebee
+     * @reason Cancel C2ME's interference
+     */
+    @Overwrite
+    public int getProgressPercentage() {
+        return MathHelper.floor((float)this.generatedCount * 100.0F / (float)this.totalCount);
     }
 }
