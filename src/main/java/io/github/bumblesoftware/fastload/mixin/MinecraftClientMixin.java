fef3df148c6613517923cbd30c68c8627df89ca0
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-07-19 01:28:59 +1000

    Code cleanup

--- src/main/java/io/github/bumblesoftware/fastload/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/MinecraftClientMixin.java
@@ -15,8 +15,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin implements MinecraftClientMixinInterface {
     //Original code is from 'kennytv, forceloadingscreen'. Code is modified to our needs.
-    @Shadow
-    public void setScreen(@Nullable Screen screen) {}
+    @Shadow public void setScreen(@Nullable Screen screen) {}
+    @Shadow private boolean windowFocused;
+    @Shadow private volatile boolean running;
+    private boolean justLoaded = false;
+    private boolean shouldLoad = false;
+    private boolean playerJoined = false;
     @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
     private void setScreen(final Screen screen, final CallbackInfo ci) {
         if (screen instanceof DownloadingTerrainScreen && shouldLoad && playerJoined && running) {
@@ -27,22 +31,6 @@ public class MinecraftClientMixin implements MinecraftClientMixinInterface {
             playerJoined  = false;
         }
     }
-    @Shadow
-    private void render(boolean tick) {}
-    @Inject(method = "startIntegratedServer", at = @At("HEAD"))
-    private void renderOnStartServer(CallbackInfo ci) {
-        render(true);
-    }
-    @Shadow
-    public boolean skipGameRender;
-    @Shadow private boolean windowFocused;
-
-    @Inject(method = "render", at = @At("HEAD"))
-    private void setSkipGameRender(boolean tick, CallbackInfo ci) {
-        skipGameRender = false;
-    }
-    private boolean justLoaded = false;
-    @Shadow private volatile boolean running;
     @Inject(method = "openPauseMenu", at = @At("HEAD"), cancellable = true)
     private void cancelOpenPauseMenu(boolean pause, CallbackInfo ci) {
         if(justLoaded) {
@@ -55,12 +43,10 @@ public class MinecraftClientMixin implements MinecraftClientMixinInterface {
             }
         }
     }
-    private boolean shouldLoad = false;
     @Override
     public void canPlayerLoad() {
         shouldLoad = true;
     }
-    private boolean playerJoined = false;
     @Override
     public void gameJoined() {
         playerJoined = true;
