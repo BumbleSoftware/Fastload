c08c48de0294d6d908269313fa3f13d396355831
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-09-08 14:20:05 +1000

    Update WorldGenerationProgressTrackerMixin.java

--- src/main/java/io/github/bumblesoftware/fastload/mixin/WorldGenerationProgressTrackerMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/WorldGenerationProgressTrackerMixin.java
@@ -1,5 +1,6 @@
 package io.github.bumblesoftware.fastload.mixin;
 
+import io.github.bumblesoftware.fastload.FLMath;
 import net.minecraft.client.gui.WorldGenerationProgressTracker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,5 +9,5 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(WorldGenerationProgressTracker.class)
 public class WorldGenerationProgressTrackerMixin {
     @ModifyVariable(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/WorldGenerationProgressLogger;<init>(I)V"), argsOnly = true)
-    public int getRadius(int radius) {return 4;}
+    public int getRadius(int radius) {return FLMath.getSetSpawnChunkRadius();}
 }
