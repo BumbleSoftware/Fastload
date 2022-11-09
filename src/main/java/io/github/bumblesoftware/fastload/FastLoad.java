693d553fc48f3e1263a277df964caf716879587c
Fluffy Bumblebee <lromanic123alt@gmail.com>
2022-11-09 13:05:16 +1100

    Fixed Server Crashes... hopefully

--- src/main/java/io/github/bumblesoftware/fastload/FastLoad.java
+++ src/main/java/io/github/bumblesoftware/fastload/FastLoad.java
@@ -1,7 +1,9 @@
 package io.github.bumblesoftware.fastload;
 
 import io.github.bumblesoftware.fastload.config.FLConfig;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,12 +14,14 @@ public class FastLoad implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		FLConfig.loadClass();
-		LOGGER.info("CHUNK_TRY_LIMIT: " + getChunkTryLimit());
-		LOGGER.info("CANCEL_LOADING_SCREEN: " + getCloseUnsafe().toString().toUpperCase());
+		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+			LOGGER.info("CHUNK_TRY_LIMIT: " + getChunkTryLimit());
+			LOGGER.info("CANCEL_LOADING_SCREEN: " + getCloseUnsafe().toString().toUpperCase());
+			LOGGER.info("PRE_RENDER_RADIUS: " + getPreRenderRadius());
+			LOGGER.info("PRE_RENDER_AREA: " + getPreRenderArea());
+		}
 		LOGGER.info("DEBUG MODE: " + getDebug().toString().toUpperCase());
 		LOGGER.info("SPAWN_CHUNK_RADIUS: " + getPregenRadius(false));
 		LOGGER.info("SPAWN CHUNK AREA: " + getPregenArea());
-		LOGGER.info("PRE_RENDER_RADIUS: " + getPreRenderRadius());
-		LOGGER.info("PRE_RENDER_AREA: " + getPreRenderArea());
 	}
 }
