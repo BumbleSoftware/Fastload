c39a2941432b502d3758007ac59c486ee3ac7fee
Fluffy Bumblebee <lromanic123alt@gmail.com>
2022-12-11 16:41:37 +1100

    Improve FLTimer

--- src/main/java/io/github/bumblesoftware/fastload/util/FLTimer.java
+++ src/main/java/io/github/bumblesoftware/fastload/util/FLTimer.java
@@ -6,10 +6,10 @@ import static io.github.bumblesoftware.fastload.client.FLClientHandler.log;
 import static io.github.bumblesoftware.fastload.config.init.FLMath.getDebug;
 
 /**
- * A simple timer that automagically registers upon creation, and once ticked down to 0, it de-registers and holds the value.
- * To use this. Make a field with this as its type, initialise it at a specific point, and then use if() statements
- * to compare your desired value & the value of getTime(). Alternatively, you can use the isReady(). Multiply your
- * time value by 20 to get the amount of seconds you want.
+ * Simple event-based timer with respect to minecraft client ticks.
+ * To use, simply set your value via setTime(), then use isReady() for your if() checks.
+ * The timer will tick down your value to 0. Each second is worth 20 ticks. Which means
+ * while your value isn't 0, isReady() will return true. Negative numbers
  */
 public final class FLTimer {
     private static int remainingTime = 0;
@@ -29,6 +29,9 @@ public final class FLTimer {
     }
 
     public static void setTime(int remainingTime) {
+        if (remainingTime < 0) {
+            throw new RuntimeException("FLTimer was called with a negative number!");
+        }
         FLTimer.remainingTime = remainingTime;
     }
 
