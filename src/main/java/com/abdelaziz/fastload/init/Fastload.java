55895f36d405c9b76cc7603ef87d09ee4eda406f
AbdElAziz333 <abdelaziz524aziz@gmail.com>
2022-12-27 09:45:45 +0200

    add @Mod annotation

--- src/main/java/com/abdelaziz/fastload/init/Fastload.java
+++ src/main/java/com/abdelaziz/fastload/init/Fastload.java
@@ -5,6 +5,7 @@ import com.abdelaziz.fastload.client.FLClientHandler;
 import com.abdelaziz.fastload.config.init.FLConfig;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.loading.FMLLoader;
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import static com.abdelaziz.fastload.config.init.DefaultConfig.propertyKeys.*;
 import static com.abdelaziz.fastload.config.init.FLMath.*;
 
+@Mod(Fastload.NAMESPACE)
 public class Fastload {
     public static final String NAMESPACE = "Fastload";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
