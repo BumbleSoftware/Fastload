82ce4d907344baf30b75fee14a7c6ff8a83f54c2
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2023-04-06 15:18:51 +1000

    Better info in building terrain

--- src/main/java/io/github/bumblesoftware/fastload/client/sceen/BuildingTerrainScreen.java
+++ src/main/java/io/github/bumblesoftware/fastload/client/sceen/BuildingTerrainScreen.java
@@ -11,12 +11,12 @@ import static io.github.bumblesoftware.fastload.init.FastloadClient.ABSTRACTED_C
 import static io.github.bumblesoftware.fastload.util.FLColourConstants.WHITE;
 
 public class BuildingTerrainScreen extends Screen {
-    private final Text SCREEN_NAME;
-    private final Text SCREEN_TEMPLATE;
-    private final Text BUILDING_CHUNKS;
-    private final Text PREPARING_CHUNKS;
-    private Integer PREPARED_PROGRESS_STORAGE = 0;
-    private Integer BUILDING_PROGRESS_STORAGE = 0;
+    private final Text screenName;
+    private final Text screenTemplate;
+    private final Text buildingChunks;
+    private final Text preparingChunks;
+    private Integer preparedProgressStorage = 0;
+    private Integer buildingProgressStorage = 0;
     private static final int heightUpFromCentre = 50;
     private Integer getLoadedChunkCount() {
         return ABSTRACTED_CLIENT.getClientWorld() != null ? ABSTRACTED_CLIENT.getLoadedChunkCount() : 0;
@@ -30,10 +30,10 @@ public class BuildingTerrainScreen extends Screen {
      */
     public BuildingTerrainScreen() {
         super(NarratorManager.EMPTY);
-        SCREEN_NAME = ABSTRACTED_CLIENT.newTranslatableText("menu.generatingTerrain");
-        SCREEN_TEMPLATE = ABSTRACTED_CLIENT.newTranslatableText("fastload.screen.buildingTerrain.template");
-        BUILDING_CHUNKS = ABSTRACTED_CLIENT.newTranslatableText("fastload.screen.buildingTerrain.building");
-        PREPARING_CHUNKS = ABSTRACTED_CLIENT.newTranslatableText("fastload.screen.buildingTerrain.preparing");
+        screenName = ABSTRACTED_CLIENT.newTranslatableText("menu.generatingTerrain");
+        screenTemplate = ABSTRACTED_CLIENT.newTranslatableText("fastload.screen.buildingTerrain.template");
+        buildingChunks = ABSTRACTED_CLIENT.newTranslatableText("fastload.screen.buildingTerrain.building");
+        preparingChunks = ABSTRACTED_CLIENT.newTranslatableText("fastload.screen.buildingTerrain.preparing");
     }
 
     /**
@@ -44,27 +44,40 @@ public class BuildingTerrainScreen extends Screen {
         ABSTRACTED_CLIENT.renderScreenBackgroundTexture(this, 0, matrices);
         final String loadedChunksString = getLoadedChunkCount() + "/"  + FLMath.getPreRenderArea();
         final String builtChunksString = getBuiltChunkCount() + "/"  + FLMath.getPreRenderArea();
-        if (PREPARED_PROGRESS_STORAGE < getLoadedChunkCount()) {
+        if (preparedProgressStorage < getLoadedChunkCount()) {
             Fastload.LOGGER.info("World Chunk Loading: " + loadedChunksString);
         }
-        if (BUILDING_PROGRESS_STORAGE < getBuiltChunkCount()) {
+        if (buildingProgressStorage < getBuiltChunkCount()) {
             Fastload.LOGGER.info("World Chunk Building: " + builtChunksString);
         }
-        PREPARED_PROGRESS_STORAGE = getLoadedChunkCount();
-        BUILDING_PROGRESS_STORAGE = getBuiltChunkCount();
+        preparedProgressStorage = getLoadedChunkCount();
+        buildingProgressStorage = getBuiltChunkCount();
+
+        if (getLoadedChunkCount() == 0 && getBuiltChunkCount() == 0) {
+            ABSTRACTED_CLIENT.drawCenteredText(
+                    matrices,
+                    this.textRenderer,
+                    ABSTRACTED_CLIENT.newTranslatableText("fastload.buildingTerrain.starting"),
+                    this.width / 2,
+                    this.height / 2 - heightUpFromCentre,
+                    WHITE
+            );
+            return;
+        }
 
         ABSTRACTED_CLIENT.drawCenteredText(
                 matrices,
                 this.textRenderer,
-                SCREEN_NAME,
+                screenName,
                 this.width / 2,
                 this.height / 2 - heightUpFromCentre,
                 WHITE
         );
+
         ABSTRACTED_CLIENT.drawCenteredText(
                 matrices,
                 this.textRenderer,
-                SCREEN_TEMPLATE,
+                screenTemplate,
                 this.width / 2,
                 this.height / 2 - heightUpFromCentre + 30,
                 WHITE
@@ -73,7 +86,7 @@ public class BuildingTerrainScreen extends Screen {
         ABSTRACTED_CLIENT.drawCenteredText(
                 matrices,
                 this.textRenderer,
-                 PREPARING_CHUNKS.getString() + ": " + loadedChunksString,
+                 preparingChunks.getString() + ": " + loadedChunksString,
                 width / 2,
                 height / 2 - heightUpFromCentre + 45,
                 WHITE);
@@ -81,7 +94,7 @@ public class BuildingTerrainScreen extends Screen {
         ABSTRACTED_CLIENT.drawCenteredText(
                 matrices,
                 this.textRenderer,
-                BUILDING_CHUNKS.getString() + ": " + builtChunksString,
+                buildingChunks.getString() + ": " + builtChunksString,
                 width / 2,
                 height / 2 - heightUpFromCentre + 60,
                 WHITE);
