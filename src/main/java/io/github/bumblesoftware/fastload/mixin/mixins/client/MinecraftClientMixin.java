47efd1b79cc0f86f603e831bbad6470bfc0a3a4e
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2023-04-06 13:29:37 +1000

    organise

--- src/main/java/io/github/bumblesoftware/fastload/mixin/mixins/client/MinecraftClientMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/mixins/client/MinecraftClientMixin.java
@@ -1,55 +1,35 @@
 package io.github.bumblesoftware.fastload.mixin.mixins.client;
 
-import io.github.bumblesoftware.fastload.client.FLClientEvents.RecordTypes.PauseMenuEventContext;
-import io.github.bumblesoftware.fastload.client.FLClientEvents.RecordTypes.SetScreenEventContext;
-import io.github.bumblesoftware.fastload.client.FLClientEvents.RecordTypes.TickEventContext;
 import io.github.bumblesoftware.fastload.config.init.FLMath;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.server.integrated.IntegratedServer;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static io.github.bumblesoftware.fastload.client.FLClientEvents.Events.*;
 import static io.github.bumblesoftware.fastload.init.FastloadClient.ABSTRACTED_CLIENT;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
-    private void setScreenEvent(final Screen screen, final CallbackInfo ci) {
-        SET_SCREEN_EVENT.fireEvent(new SetScreenEventContext(screen, ci));
-    }
-
-    @Inject(method = "openPauseMenu", at = @At("HEAD"), cancellable = true)
-    private void openPauseMenuEvent(boolean pause, CallbackInfo ci) {
-        PAUSE_MENU_EVENT.fireEvent(new PauseMenuEventContext(pause, ci));
-
-    }
-
-    @Inject(method = "render", at = @At("HEAD"))
-    private void renderEvent(boolean tick, CallbackInfo ci) {
-        RENDER_TICK_EVENT.fireEvent(new TickEventContext(tick));
-    }
-
     @Redirect(method = "startIntegratedServer(Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Function;" +
             "ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V", at = @At(value = "INVOKE", target = "Lnet" +
             "/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 2))
     private void remove441(MinecraftClient client, @Nullable Screen screen) {
         if (FLMath.isPreRenderEnabled())
             client.setScreen(ABSTRACTED_CLIENT.newBuildingTerrainScreen());
-    }
-
-    @Redirect(method = "joinWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;reset(Lnet/minecraft/client/gui/screen/Screen;)V"))
-    private void removeProgressScreen(MinecraftClient client, Screen screen) {
+        else client.setScreenAndRender(new DownloadingTerrainScreen());
     }
 
     @Redirect(method = "startIntegratedServer(Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Function;" +
             "ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;isLoading()Z"))
     private boolean removeWait(IntegratedServer integratedServer) {
         return true;
+    }
+
+    @Redirect(method = "joinWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;reset(Lnet/minecraft/client/gui/screen/Screen;)V"))
+    private void removeProgressScreen(MinecraftClient client, Screen screen) {
     }
 }
--- src/main/java/io/github/bumblesoftware/fastload/mixin/mixins/server/MinecraftServerMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/mixins/server/MinecraftServerMixin.java
@@ -1,17 +1,14 @@
 package io.github.bumblesoftware.fastload.mixin.mixins.server;
 
-import io.github.bumblesoftware.fastload.client.FLClientEvents.RecordTypes.TickEventContext;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.util.math.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.BooleanSupplier;
-
-import static io.github.bumblesoftware.fastload.client.FLClientEvents.Events.SERVER_TICK_EVENT;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 
 /*
@@ -19,9 +16,6 @@ import static io.github.bumblesoftware.fastload.client.FLClientEvents.Events.SER
 * The BumbleSoftware team modified the code.
 */
 
-/**
- * Used to change how many chunks should load at 441.
- */
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
 
@@ -36,10 +30,5 @@ public abstract class MinecraftServerMixin {
     private void finishEarly(WorldGenerationProgressListener worldGenerationProgressListener, ChunkPos chunkPos) {
         worldGenerationProgressListener.stop();
         updateMobSpawnOptions();
-    }
-
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void onTick(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
-        SERVER_TICK_EVENT.fireEvent(new TickEventContext(shouldKeepTicking.getAsBoolean()));
     }
 }
