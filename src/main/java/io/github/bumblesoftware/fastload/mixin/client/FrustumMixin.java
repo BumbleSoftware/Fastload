3976c73b49c6069c2888eb28224cc634c1fcc38b
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-08-15 13:01:39 +0300

    Change arch Forge Loom to Forge

--- src/main/java/io/github/bumblesoftware/fastload/mixin/client/FrustumMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/client/FrustumMixin.java
@@ -1,8 +1,8 @@
 package io.github.bumblesoftware.fastload.mixin.client;
 
 import io.github.bumblesoftware.fastload.client.FLClientEvents.Contexts.BoxBooleanContext;
-import net.minecraft.client.render.Frustum;
-import net.minecraft.util.math.Box;
+import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,14 +15,14 @@ import static io.github.bumblesoftware.fastload.client.FLClientEvents.Locations.
 
 @Mixin(Frustum.class)
 public class FrustumMixin {
-    @Inject(method = "isVisible(DDDDDD)Z", at = @At("HEAD"), cancellable = true)
-    private void setVisible(float x1, float y1, float z1, float x2, float y2, float z2, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "cubeInFrustum(DDDDDD)Z", at = @At("HEAD"), cancellable = true)
+    private void setVisible(double minX, double minY, double minZ, double maxX, double maxY, double maxZ, CallbackInfoReturnable<Boolean> cir) {
         if (BOX_BOOLEAN_EVENT.isNotEmpty(FRUSTUM_BOX_BOOL))
             BOX_BOOLEAN_EVENT.execute(
                     List.of(FRUSTUM_BOX_BOOL),
                     new BoxBooleanContext(
-                        new Box(x1, y1, z1, x2, y2, z2),
-                        cir
+                            new AABB(minX, minY, minZ, maxX, maxY, maxZ),
+                            cir
                     )
             );
     }
