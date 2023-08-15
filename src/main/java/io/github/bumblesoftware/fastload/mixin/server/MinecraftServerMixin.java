90630fe7fc0ab7a4d10868593013281460c0330e
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-08-15 13:52:00 +0300

    change from arch forge loom to forge

--- src/main/java/io/github/bumblesoftware/fastload/mixin/server/MinecraftServerMixin.java
+++ src/main/java/io/github/bumblesoftware/fastload/mixin/server/MinecraftServerMixin.java
@@ -3,8 +3,8 @@ package io.github.bumblesoftware.fastload.mixin.server;
 import io.github.bumblesoftware.fastload.common.FLCommonEvents;
 import io.github.bumblesoftware.fastload.util.obj_holders.MutableObjectHolder;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.WorldGenerationProgressListener;
-import net.minecraft.util.math.ChunkPos;
+import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.world.level.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,22 +16,20 @@ import static io.github.bumblesoftware.fastload.common.FLCommonEvents.Events.*;
 import static io.github.bumblesoftware.fastload.common.FLCommonEvents.Locations.PREPARE_START_REGION;
 import static io.github.bumblesoftware.fastload.common.FLCommonEvents.Locations.SERVER_TICK;
 
-
 /*
 * This code is inspired by: https://github.com/VidTu/Ksyxis of which it's under the MIT License.
 * The BumbleSoftware team modified the code.
 */
-
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
 
-    @Inject(method = "tick", at = @At("HEAD"))
+    @Inject(method = "tickServer", at = @At("HEAD"))
     private void onTick(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
         if (BOOLEAN_EVENT.isNotEmpty(PREPARE_START_REGION))
             BOOLEAN_EVENT.execute(List.of(SERVER_TICK), new MutableObjectHolder<>(shouldKeepTicking.getAsBoolean()));
     }
 
-    @ModifyConstant(method = "prepareStartRegion", constant = @Constant(intValue = 441))
+    @ModifyConstant(method = "prepareLevels", constant = @Constant(intValue = 441))
     private int modify_prepareStartRegion_chunkCount(int value) {
         final var returnValue = new MutableObjectHolder<>(441);
         if (INTEGER_EVENT.isNotEmpty(PREPARE_START_REGION))
@@ -39,8 +37,8 @@ public abstract class MinecraftServerMixin {
         return returnValue.getHeldObj();
     }
 
-    @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/WorldGenerationProgressListener;start(Lnet/minecraft/util/math/ChunkPos;)V"))
-    private void handleProgressListener(WorldGenerationProgressListener worldGenerationProgressListener, ChunkPos chunkPos) {
+    @Redirect(method = "prepareLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/progress/ChunkProgressListener;updateSpawnPos(Lnet/minecraft/world/level/ChunkPos;)V"))
+    private void handleProgressListener(ChunkProgressListener worldGenerationProgressListener, ChunkPos chunkPos) {
         if (PROGRESS_LISTENER_EVENT.isNotEmpty(PREPARE_START_REGION))
             PROGRESS_LISTENER_EVENT.execute(
                     new FLCommonEvents.Contexts.ProgressListenerContext(worldGenerationProgressListener, chunkPos)
